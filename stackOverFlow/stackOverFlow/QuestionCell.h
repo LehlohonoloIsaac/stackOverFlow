466db253a376a6b28bac7aad3840df957f81c8ea
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-08-17 16:01:45 +0200

    Add an xib file and class for QuestionCell

---
+++ stackOverFlow/stackOverFlow/QuestionCell.h
@@ -0,0 +1,13 @@
+//
+//  QuestionCell.h
+//  stackOverFlow
+//
+//  Created by DVT on 2017/08/17.
+//  Copyright © 2017 DVT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionCell : UITableViewCell
+
+@end
---
+++ stackOverFlow/stackOverFlow/QuestionCell.m
@@ -0,0 +1,24 @@
+//
+//  QuestionCell.m
+//  stackOverFlow
+//
+//  Created by DVT on 2017/08/17.
+//  Copyright © 2017 DVT. All rights reserved.
+//
+
+#import "QuestionCell.h"
+
+@implementation QuestionCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
