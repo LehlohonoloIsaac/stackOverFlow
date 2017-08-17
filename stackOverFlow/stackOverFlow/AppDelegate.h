2c124450edad71bfd5ffe23e3a308263a4e0d486
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-08-17 10:32:19 +0200

    Add stackOverFlow project

---
+++ stackOverFlow/stackOverFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stackOverFlow
+//
+//  Created by DVT on 2017/08/17.
+//  Copyright © 2017 DVT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
