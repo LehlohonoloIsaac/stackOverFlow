c6becbe20794686b1dd7f002c9eaa9fa4d36774a
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-08-30 11:03:31 +0200

    Add property for accepted_answer_id

--- stackOverFlow/stackOverFlow/Question.h
+++ stackOverFlow/stackOverFlow/Question.h
@@ -16,6 +16,7 @@
 @property (nonatomic,retain) NSDate *creation_date;
 @property (nonatomic,assign) Boolean isAnswerAccepted;
 @property (nonatomic,assign) Boolean is_answered;
+@property (nonatomic,assign) NSUInteger accepted_answer_id;
 
 -(instancetype)initWithTitle:(NSString *)title numberOfAnswers:(NSUInteger )numberOfAnswers tags:(NSArray *)tags timeAgo:(NSDate *)timeAgo isAnswerAccepted:(Boolean)isAnswerAccepted isQuestionAnswered:(Boolean)isAnswered;
 
