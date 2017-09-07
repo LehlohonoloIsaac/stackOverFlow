2a2b78b4eb874a7b95279ec5194c8201e75bfb29
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-09-07 13:53:16 +0200

    Remove unused methods in QuestionsListViewModel

--- stackOverFlow/stackOverFlow/QuestionsListViewModel.m
+++ stackOverFlow/stackOverFlow/QuestionsListViewModel.m
@@ -40,61 +40,6 @@
     return _questions[indexPath.row];
 }
 
--(NSArray *)tagsAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.tags;
-}
 
--(NSString *)timeAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    long hoursPosted = [self convertTimeToHours:question.creation_date];
-    long now = [self convertTimeToHours:[NSDate date]];
-    long hoursAgo = labs(now-hoursPosted);
-    return hoursAgo==1 ? [NSString stringWithFormat:@"%ld %@",hoursAgo,@"hour ago"]: [NSString stringWithFormat:@"%ld %@",hoursAgo,@"hours ago"];
-}
-
--(NSUInteger)numberOfAnswersAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.answer_count;
-}
-
--(NSString *)displayQuestionAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.title;
-}
-
--(Boolean)isAnswerAccepted:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.isAnswerAccepted;
-}
-
--(NSString *)setAnswerLabelAtIndexPath:(NSIndexPath *)indexPath{
-    return [self numberOfAnswersAtIndexPath:indexPath] == 1 ? @"Answer" : @"Answers";
-}
-
--(UIColor *)setBackgroundColorForAnswerHolderAtIndexPath:(NSIndexPath *)indexPath{
-    return [self questionHasAcceptedAnswerAtIndexPath:indexPath]? UIColor.greenColor : [UIColor.lightGrayColor colorWithAlphaComponent:0.4];
-}
-
--(NSDate *)getCurrentTime{
-    NSDate *now = [[NSDate alloc]init];
-    return now;
-}
-
--(NSDate *)getTimeAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.creation_date;
-}
-
--(long)convertTimeToHours:(NSDate *)date{
-    NSString *creationDate = [NSString stringWithFormat:@"%@",date];
-    long creationDateUnixTimestamp = (unsigned long)creationDate;
-    return creationDateUnixTimestamp/(1000*60*60);
-}
-
--(BOOL)questionHasAcceptedAnswerAtIndexPath:(NSIndexPath *)indexPath{
-    Question *question = (Question *)[self questionAtIndexPath:indexPath];
-    return question.accepted_answer_id != 0? true: false;
-}
 
 @end
